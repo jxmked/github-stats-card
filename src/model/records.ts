95581be0faa544628a251a82773a4d401c885580
jxmked <jovandeguia@gmail.com>
2023-03-22 12:06:30 +0800

    update

--- src/model/records.ts
+++ src/model/records.ts
@@ -1,5 +1,15 @@
 import Fetcher from './fetcher';
 
+/**
+ * After account validation,
+ * user info, stats, and other required data
+ * will be stored here and will be set as READONLY
+ * 
+ * During rendering, options from url will be parse and analyze
+ * to return the expected result
+ * 
+ * */
+ 
 export default class Records {
   protected ff: Fetcher;
 
